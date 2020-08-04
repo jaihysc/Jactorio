jaihysc/Jactorio

commit 20c4eaa5b80465afe3de949b942802ca08bfdc99

Inserter behavior no longer fluctuates based on logic update order
Previously, depending on the order where inserters are updated, it may pickup an item dropped off on the same tick, or the next tick

src/game/logic/inserter_controller.cpp
@@ -2,6 +2,8 @@
 
 #include "game/logic/inserter_controller.h"
 
+#include <vector>
+
 #include "data/prototype/entity/inserter.h"
 
 using namespace jactorio;
@@ -24,54 +26,49 @@ double game::GetInserterArmLength(const core::TIntDegree degree, const uint8_t t
 }
 
 
-void InserterUpdate(game::LogicData& logic_data,
-                    const data::Inserter& inserter_proto, data::InserterData& inserter_data) {
+// ======================================================================
+
+
+// Inserters are updated in stages together so that logic order does not affect inserter behavior
+// E.g Inserter1 drop off             , inserter2 picks up | 1 logic update
+//     Inserter2 has nothing to pickup, inserter1 drop off | 1 logic update
+
+struct InserterUpdateProps
+{
+	const data::Inserter& proto;
+	data::InserterData& data;
+};
+
+using DropoffQueue = std::vector<InserterUpdateProps>;
+using PickupQueue = std::vector<InserterUpdateProps>;
+
+///
+/// \brief Rotates inserters, queues inserters awaiting dropoff and pickup handling
+void RotateInserters(DropoffQueue& dropoff_queue, PickupQueue& pickup_queue,
+                     const InserterUpdateProps& props) {
 	using namespace game;
 
-	assert(inserter_proto.rotationSpeed.getAsDouble() != 0);
+	assert(props.proto.rotationSpeed.getAsDouble() != 0);
 
-	switch (inserter_data.status) {
+	switch (props.data.status) {
 
 	case data::InserterData::Status::dropoff:
-		inserter_data.rotationDegree -= inserter_proto.rotationSpeed;
-		if (inserter_data.rotationDegree <= data::ToRotationDegree(kMinInserterDegree)) {
-			inserter_data.rotationDegree = 0;  // Prevents underflow if the inserter sits idle for a long time
+		props.data.rotationDegree -= props.proto.rotationSpeed;
 
-			if (inserter_data.dropoff.DropOff(logic_data, inserter_data.heldItem)) {
-				inserter_data.status = data::InserterData::Status::pickup;
-			}
+		if (props.data.rotationDegree <= data::ToRotationDegree(kMinInserterDegree)) {
+			props.data.rotationDegree = 0;  // Prevents underflow if the inserter sits idle for a long time
+			dropoff_queue.push_back(props);
 		}
 
 		return;
 
 	case data::InserterData::Status::pickup:
 		// Rotate the inserter
-		inserter_data.rotationDegree += inserter_proto.rotationSpeed;
+		props.data.rotationDegree += props.proto.rotationSpeed;
 
-		if (inserter_data.rotationDegree > data::ToRotationDegree(kMaxInserterDegree)) {
-			inserter_data.rotationDegree = kMaxInserterDegree;
-
-			constexpr int pickup_amount = 1;
-
-			const auto* to_be_picked_item =
-				inserter_data.pickup.GetPickup(logic_data,
-				                               inserter_proto.tileReach,
-				                               inserter_data.rotationDegree);
-
-			// Do not pick up item if it cannot be dropped off
-			if (!inserter_data.dropoff.CanDropOff(logic_data, to_be_picked_item))
-				return;
-
-
-			const auto result = inserter_data.pickup.Pickup(logic_data,
-			                                                inserter_proto.tileReach,
-			                                                inserter_data.rotationDegree,
-			                                                pickup_amount);
-			if (result.first) {
-				inserter_data.heldItem = result.second;
-
-				inserter_data.status = data::InserterData::Status::dropoff;
-			}
+		if (props.data.rotationDegree > data::ToRotationDegree(kMaxInserterDegree)) {
+			props.data.rotationDegree = kMaxInserterDegree;  // Prevents overflow
+			pickup_queue.push_back(props);
 		}
 
 		return;
@@ -79,9 +76,54 @@ void InserterUpdate(game::LogicData& logic_data,
 	default:
 		assert(false);
 	}
+
+}
+
+void ProcessInserterDropoff(const DropoffQueue& dropoff_queue, game::LogicData& logic_data) {
+	for (const auto& inserter_prop : dropoff_queue) {
+		auto& inserter_data = inserter_prop.data;
+
+		if (inserter_data.dropoff.DropOff(logic_data, inserter_data.heldItem)) {
+			inserter_data.status = data::InserterData::Status::pickup;
+		}
+	}
+
+}
+
+void ProcessInserterPickup(const PickupQueue& pickup_queue, game::LogicData& logic_data) {
+	for (const auto& inserter_prop : pickup_queue) {
+		auto& inserter_data        = inserter_prop.data;
+		const auto& inserter_proto = inserter_prop.proto;
+
+		constexpr int pickup_amount = 1;
+
+		const auto* to_be_picked_item =
+			inserter_data.pickup.GetPickup(logic_data,
+			                               inserter_proto.tileReach,
+			                               inserter_data.rotationDegree);
+
+		// Do not pick up item if it cannot be dropped off
+		if (!inserter_data.dropoff.CanDropOff(logic_data, to_be_picked_item))
+			return;
+
+
+		const auto result = inserter_data.pickup.Pickup(logic_data,
+		                                                inserter_proto.tileReach,
+		                                                inserter_data.rotationDegree,
+		                                                pickup_amount);
+		if (result.first) {
+			inserter_data.heldItem = result.second;
+
+			inserter_data.status = data::InserterData::Status::dropoff;
+		}
+	}
+
 }
 
 void game::InserterLogicUpdate(WorldData& world_data, LogicData& logic_data) {
+	DropoffQueue dropoff_queue{};
+	PickupQueue pickup_queue{};
+
 	for (auto* chunk : world_data.LogicGetChunks()) {
 		for (auto* tile_layer : chunk->GetLogicGroup(Chunk::LogicGroup::inserter)) {
 			auto* inserter_data = tile_layer->GetUniqueData<data::InserterData>();
@@ -90,7 +132,10 @@ void game::InserterLogicUpdate(WorldData& world_data, LogicData& logic_data) {
 			const auto* proto_data = tile_layer->GetPrototypeData<data::Inserter>();
 			assert(proto_data);
 
-			InserterUpdate(logic_data, *proto_data, *inserter_data);
+			RotateInserters(dropoff_queue, pickup_queue, {*proto_data, *inserter_data});
 		}
 	}
+
+	ProcessInserterDropoff(dropoff_queue, logic_data);
+	ProcessInserterPickup(pickup_queue, logic_data);
 }

test/game/logic/inserter_controllerTests.cpp
@@ -33,25 +33,30 @@ namespace jactorio::game
 		}
 
 		///
-		/// \brief Creates chest with 10 items, emits OnNeighborUpdate for 1, 2
+		/// \brief Creates chest with, emits OnNeighborUpdate
 		/// \param orientation Orientation to chest from inserter
+		/// \param stack_count Amount of items chest starts with
 		data::ContainerEntityData* BuildChest(const WorldCoord& coords,
-		                                      const data::Orientation orientation) {
+		                                      const data::Orientation orientation,
+		                                      const data::Item::StackCount stack_count,
+		                                      const WorldCoord& neighbor_update_coord = {1, 2}) {
 			auto& layer = TestSetupContainer(worldData_, coords, containerProto_);
 
 			auto* unique_data = layer.GetUniqueData<data::ContainerEntityData>();
 
+			// Emit neighbor update
 			{
-				auto& neighbor_layer       = worldData_.GetTile({1, 2})->GetLayer(ChunkTile::ChunkLayer::entity);
+				auto& neighbor_layer       = worldData_.GetTile(neighbor_update_coord)->GetLayer(ChunkTile::ChunkLayer::entity);
 				const auto* neighbor_proto =
 					static_cast<const data::ContainerEntity*>(neighbor_layer.prototypeData);
 
 				if (neighbor_proto)
 					neighbor_proto->OnNeighborUpdate(worldData_, logicData_,
-					                                 coords, {1, 2}, orientation);
+					                                 coords, neighbor_update_coord, orientation);
 			}
 
-			unique_data->inventory[0] = {&containerItemProto_, 10};
+			if (stack_count != 0)
+				unique_data->inventory[0] = {&containerItemProto_, stack_count};
 
 			return unique_data;
 		}
@@ -81,8 +86,8 @@ namespace jactorio::game
 		inserterProto_.rotationSpeed = 2.1;
 		const int updates_to_target  = 86;
 
-		auto* dropoff = BuildChest({0, 2}, data::Orientation::left);  // Dropoff
-		auto* pickup  = BuildChest({2, 2}, data::Orientation::right);  // Pickup
+		auto* dropoff = BuildChest({0, 2}, data::Orientation::left, 10);
+		auto* pickup  = BuildChest({2, 2}, data::Orientation::right, 10);
 
 		auto& inserter_layer = BuildInserter({1, 2}, data::Orientation::left);
 		auto* inserter_data  = inserter_layer.GetUniqueData<data::InserterData>();
@@ -170,9 +175,9 @@ namespace jactorio::game
 
 		// Cannot drop into assembly machine since it has no recipe
 		const data::AssemblyMachine asm_machine{};
-		auto& dropoff = TestSetupAssemblyMachine(worldData_, {0, 1}, asm_machine);
+		TestSetupAssemblyMachine(worldData_, {0, 1}, asm_machine);
 
-		auto* pickup = BuildChest({3, 2}, data::Orientation::right);
+		auto* pickup = BuildChest({3, 2}, data::Orientation::right, 10);
 
 
 		inserterProto_.rotationSpeed = 2.1f;
@@ -185,5 +190,32 @@ namespace jactorio::game
 
 		EXPECT_EQ(inserter_data->status, data::InserterData::Status::pickup);
 		EXPECT_EQ(pickup->inventory[0].count, 10);
+	}
+
+	TEST_F(InserterControllerTest, PickupDroppedoffItem) {
+		// Can pickup an item dropped off by another inserter on the same game tick
+
+		inserterProto_.rotationSpeed = 180.f;
+
+		auto* left_chest  = BuildChest({0, 2}, data::Orientation::up, 1);
+		auto* mid_chest   = BuildChest({2, 2}, data::Orientation::up, 0);
+		auto* right_chest = BuildChest({4, 2}, data::Orientation::up, 0);
+
+		// Order which inserters are updated should not matter
+		BuildInserter({3, 2}, data::Orientation::right);
+		BuildInserter({1, 2}, data::Orientation::right);
+
+
+		// Pickup
+		InserterLogicUpdate(worldData_, logicData_);
+		EXPECT_EQ(left_chest->inventory[0].count, 0);
+
+		// Dropoff, picked up by inserter2
+		InserterLogicUpdate(worldData_, logicData_);
+		EXPECT_EQ(mid_chest->inventory[0].count, 0);
+
+		// Dropoff by inserter2
+		InserterLogicUpdate(worldData_, logicData_);
+		EXPECT_EQ(right_chest->inventory[0].count, 1);
 	}
 }

commit dec63cfade92bfa7936be4d3b0ff776c9dd77f7b

Fixed all inserter updates being skipped if one inserter fails to drop off
Introduced in 20c4eaa5b80465afe3de949b942802ca08bfdc99

src/game/logic/inserter_controller.cpp
@@ -104,7 +104,7 @@ void ProcessInserterPickup(const PickupQueue& pickup_queue, game::LogicData& log
 
 		// Do not pick up item if it cannot be dropped off
 		if (!inserter_data.dropoff.CanDropOff(logic_data, to_be_picked_item))
-			return;
+			continue;
 
 
 		const auto result = inserter_data.pickup.Pickup(logic_data,