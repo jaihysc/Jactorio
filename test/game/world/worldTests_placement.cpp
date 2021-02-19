839c20f9aba41f675a1f5bd7d1e9e96b8650494b
Johnny Cai <jaihysc@gmail.com>
2021-02-19 17:21:18 -0500

    Clean up world placement tests
    - Remove unnecessary heap allocation
    - Extract duplicate code
    - Correct invalid checks for no prototype at tile (x, y was wrong)

--- test/game/world/worldTests_placement.cpp
+++ test/game/world/worldTests_placement.cpp
@@ -4,24 +4,14 @@
 
 #include "game/world/world.h"
 
-#include <memory>
-
 #include "proto/container_entity.h"
 #include "proto/tile.h"
 
 namespace jactorio::game
 {
-    // NOTE
-    // !Checking if the selected tile is too far from the player is not done in these tests!
-    //
-
     // Creates world and generates a test world within it
     class WorldPlacementTest : public testing::Test
     {
-    public:
-        static constexpr auto kChunkWidth = World::kChunkWidth;
-
-    private:
         static void GenerateTestWorld(World& world, proto::Tile* water_tile, proto::Tile* land_tile) {
             // Generates a quarter chunk on which to test entity placement (16 x 16)
             // Following indices begin at 0:
@@ -54,14 +44,14 @@ namespace jactorio::game
 
             auto& chunk = world.EmplaceChunk({0, 0});
 
-            for (ChunkTileCoordAxis y = 0; y < kChunkWidth; ++y) {
+            for (ChunkTileCoordAxis y = 0; y < Chunk::kChunkWidth; ++y) {
                 bool y_water = false;
 
                 // Water at Y index 1, 4, 8
                 if (y == 1 || y == 4 || y == 8)
                     y_water = true;
 
-                for (ChunkTileCoordAxis x = 0; x < kChunkWidth; ++x) {
+                for (ChunkTileCoordAxis x = 0; x < Chunk::kChunkWidth; ++x) {
                     bool x_water = false;
 
                     // Water at X index 1, 4, 8
@@ -79,252 +69,166 @@ namespace jactorio::game
         }
 
     protected:
-        std::unique_ptr<proto::Tile> waterTile_ = std::make_unique<proto::Tile>();
-        std::unique_ptr<proto::Tile> landTile_  = std::make_unique<proto::Tile>();
+        proto::ContainerEntity entity_;
+
+        proto::Tile waterTile_;
+        proto::Tile landTile_;
 
         World world_;
 
         void SetUp() override {
-            GenerateTestWorld(world_, waterTile_.get(), landTile_.get());
+            GenerateTestWorld(world_, &waterTile_, &landTile_);
+        }
+
+
+        ///
+        /// Checks that entity with top left at coord with dimensions has been placed
+        void ValidatePlaced(const WorldCoord& coord,
+                            const proto::Entity& entity,
+                            const proto::FWorldObject::Dimension& dimension = {1, 1}) {
+            int entity_index = 0;
+            for (WorldCoordAxis y = coord.y; y < coord.y + dimension.y; ++y) {
+                for (WorldCoordAxis x = coord.x; x < coord.x + dimension.x; ++x) {
+                    auto* tile = world_.GetTile({x, y}, TileLayer::entity);
+
+                    ASSERT_NE(tile, nullptr);
+                    EXPECT_EQ(tile->GetPrototype(), &entity);
+                    EXPECT_EQ(tile->GetMultiTileIndex(), entity_index++);
+
+                    // Ensure tile width and height are properly set
+                    EXPECT_EQ(tile->GetDimensions().span, dimension.x);
+                    EXPECT_EQ(tile->GetDimensions().height, dimension.y);
+                }
+            }
+        }
+
+        ///
+        /// Checks that entity with top left at coord with dimensions has been deleted
+        void ValidateEmpty(const WorldCoord& coord, const proto::FWorldObject::Dimension& dimension = {1, 1}) {
+            for (WorldCoordAxis y = coord.y; y < coord.y + dimension.y; ++y) {
+                for (WorldCoordAxis x = coord.x; x < coord.x + dimension.x; ++x) {
+                    auto* tile = world_.GetTile({x, y}, TileLayer::entity);
+
+                    ASSERT_NE(tile, nullptr);
+                    EXPECT_EQ(tile->GetPrototype(), nullptr);
+                    // Entity index is undefined since no entity exists now
+                }
+            }
         }
     };
 
 
     TEST_F(WorldPlacementTest, PlaceEntity1x1Valid) {
-        // Place an entity at various locations, checking that it does not place on invalid tiles
-        const auto entity = std::make_unique<proto::ContainerEntity>();
+        EXPECT_TRUE(world_.Place({0, 0}, Orientation::up, &entity_));
 
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-        EXPECT_TRUE(world_.Place({0, 0}, Orientation::up, entity.get()));
-
-        EXPECT_EQ(chunk->Tiles(TileLayer::entity)[0].GetPrototype(), entity.get());
-
-        EXPECT_FALSE(chunk->Tiles(TileLayer::entity)[0].IsMultiTile());
+        ValidatePlaced({0, 0}, entity_);
     }
 
     TEST_F(WorldPlacementTest, PlaceEntity1x1Invalid) {
-        // Place an entity at various locations, checking that it does not place on invalid tiles
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-
         // Invalid, placing on a base tile which is water
-        const auto* chunk = world_.GetChunkC({0, 0});
+        EXPECT_FALSE(world_.Place({1, 0}, Orientation::up, &entity_));
 
-        EXPECT_FALSE(world_.Place({1, 0}, Orientation::up, entity.get()));
-        EXPECT_EQ(chunk->Tiles(TileLayer::entity)[0].GetPrototype(), nullptr);
+        ValidateEmpty({1, 0});
     }
 
 
     TEST_F(WorldPlacementTest, RemoveEntity1x1Valid) {
-        // An existing tile location should have its entity and tile sprite pointer set to nullptr
-        // To remove, pass a nullptr as entity
-        const auto entity = std::make_unique<proto::ContainerEntity>();
+        EXPECT_TRUE(world_.Place({10, 9}, Orientation::up, &entity_));
+        EXPECT_TRUE(world_.Place({10, 9}, Orientation::up, nullptr));
 
-
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-        // Place entity, taken from the test above (place_entity_1x1_valid)
-        {
-            // const auto* chunk = world.get_chunk(0, 0);
-
-            EXPECT_TRUE(world_.Place({0, 0}, Orientation::up, entity.get()));
-
-            EXPECT_EQ(chunk->Tiles(TileLayer::entity)[0].GetPrototype(), entity.get());
-        }
-
-        // Valid Removal
-        EXPECT_TRUE(world_.Place({0, 0}, Orientation::up, nullptr));
-
-        // Should all be nullptr after being removed
-        EXPECT_EQ(chunk->Tiles(TileLayer::entity)[0].GetPrototype(), nullptr);
+        ValidateEmpty({10, 9});
     }
 
-    TEST_F(WorldPlacementTest, RemoveEntity1x1Invalid) {
-        // Removing a location with nullptr entity and sprite does nothing, returns false to indicate nothing was
-        // removed
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-        // Invalid Removal
-        EXPECT_FALSE(world_.Place({0, 0}, Orientation::up, nullptr));
-
-        // Should all remain nullptr
-        EXPECT_EQ(chunk->Tiles(TileLayer::entity)[0].GetPrototype(), nullptr);
+    TEST_F(WorldPlacementTest, RemoveNonExistentEntity1x1) {
+        EXPECT_FALSE(world_.Place({11, 9}, Orientation::up, nullptr));
+        ValidateEmpty({11, 9});
     }
 
+
+    // Multi tile
 
     TEST_F(WorldPlacementTest, PlaceEntity3x3Valid) {
-        // For entities spanning > 1 tiles, the given location is the top left of the entity
+        const proto::FWorldObject::Dimension dimension(3, 3);
+        entity_.SetDimension(dimension);
 
-        // Place an entity at various locations, checking that it does not place on invalid tiles
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(3);
+        EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, &entity_));
 
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-
-        EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, entity.get()));
-
-        int entity_index = 0;
-        for (int y = 5; y < 5 + 3; ++y) {
-            for (int x = 5; x < 5 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), entity.get());
-
-                // Should count up according to the rules specified in entity_index
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetMultiTileIndex(), entity_index++);
-            }
-        }
+        ValidatePlaced({5, 5}, entity_, dimension);
     }
 
-    TEST_F(WorldPlacementTest, PlaceEntity3x3Invalid1) {
-        // For entities spanning > 1 tiles, the given location is the top left of the entity
+    TEST_F(WorldPlacementTest, PlaceEntity2x3InvalidTopLeft) {
+        const proto::FWorldObject::Dimension dimension(2, 3);
+        entity_.SetDimension(dimension);
 
-        // Place an entity at various locations, checking that it does not place on invalid tiles
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(3);
+        // x = 4 is invalid tile
+        EXPECT_FALSE(world_.Place({4, 5}, Orientation::up, &entity_));
 
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-
-        EXPECT_FALSE(world_.Place({4, 5}, Orientation::up, entity.get()));
-
-        for (int y = 5; y < 5 + 3; ++y) {
-            for (int x = 5; x < 5 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), nullptr);
-            }
-        }
+        ValidateEmpty({4, 5}, dimension);
     }
 
-    TEST_F(WorldPlacementTest, PlaceEntity3x3Invalid2) {
-        // For entities spanning > 1 tiles, the given location is the top left of the entity
+    TEST_F(WorldPlacementTest, PlaceEntity3x3InvalidNonTopLeft) {
+        const proto::FWorldObject::Dimension dimension(3, 3);
+        entity_.SetDimension(dimension);
 
-        // Place an entity at various locations, checking that it does not place on invalid tiles
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(3);
+        // y = 1 is invalid tile
+        EXPECT_FALSE(world_.Place({9, 2}, Orientation::up, &entity_));
 
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-
-        EXPECT_FALSE(world_.Place({9, 2}, Orientation::up, entity.get()));
-
-        for (int y = 5; y < 5 + 3; ++y) {
-            for (int x = 5; x < 5 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), nullptr);
-            }
-        }
+        ValidateEmpty({9, 2}, dimension);
     }
 
-    TEST_F(WorldPlacementTest, PlaceEntity3x3Invalid3) {
-        // When the placed entity overlaps another entity, the placement is also invalid
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(3);
+    ///
+    /// When the placed entity overlaps another entity, the placement is also invalid
+    TEST_F(WorldPlacementTest, PlaceEntity3x3OverlapExisting) {
+        const proto::FWorldObject::Dimension dimension(3, 3);
+        entity_.SetDimension(dimension);
 
-
-        EXPECT_TRUE(world_.Place({9, 10}, Orientation::up, entity.get()));
-        EXPECT_FALSE(world_.Place({9, 9}, Orientation::up, entity.get()));
+        EXPECT_TRUE(world_.Place({9, 10}, Orientation::up, &entity_));
+        EXPECT_FALSE(world_.Place({9, 9}, Orientation::up, &entity_));
     }
 
-    TEST_F(WorldPlacementTest, RemoveEntity3x3Valid1) {
-        // When removing an entity, specifying anywhere will remove the entire entity
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(3);
 
-        const auto* chunk = world_.GetChunkC({0, 0});
+    TEST_F(WorldPlacementTest, RemoveEntity3x3Valid) {
+        const proto::FWorldObject::Dimension dimension(3, 3);
+        entity_.SetDimension(dimension);
 
-
-        EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, entity.get()));
+        EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, &entity_));
         EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, nullptr));
 
-        // Check that it has been deleted
-        for (int y = 5; y < 5 + 3; ++y) {
-            for (int x = 5; x < 5 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), nullptr);
-
-                // Entity index is undefined since no entity exists now
-            }
-        }
+        ValidateEmpty({5, 5}, dimension);
     }
 
-    TEST_F(WorldPlacementTest, RemoveEntity3x3Valid2) {
-        // When removing an entity, specifying anywhere will remove the entire entity
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(3);
+    ///
+    /// When removing an entity, specifying anywhere will remove the entire entity
+    TEST_F(WorldPlacementTest, RemoveEntity3x3ValidSelectNonTopLeft) {
+        const proto::FWorldObject::Dimension dimension(3, 3);
+        entity_.SetDimension(dimension);
 
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-
-        EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, entity.get()));
+        EXPECT_TRUE(world_.Place({5, 5}, Orientation::up, &entity_));
         EXPECT_TRUE(world_.Place({7, 5}, Orientation::up, nullptr));
-        // Check that it has been deleted
-        for (int y = 5; y < 5 + 3; ++y) {
-            for (int x = 5; x < 5 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), nullptr);
 
-                // Entity index is undefined since no entity exists now
-            }
-        }
+        ValidateEmpty({5, 5}, dimension);
     }
 
 
     // 3 x 4
 
     TEST_F(WorldPlacementTest, PlaceEntity3x4Valid) {
-        // Ensure that irregular shaped multi-tiles fully remove
+        const proto::FWorldObject::Dimension dimension(3, 4);
+        entity_.SetDimension(dimension);
 
-        // For entities spanning > 1 tiles, the given location is the top left of the entity
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(4);
+        EXPECT_TRUE(world_.Place({9, 10}, Orientation::up, &entity_));
 
-        const auto* chunk = world_.GetChunkC({0, 0});
-
-
-        EXPECT_TRUE(world_.Place({9, 10}, Orientation::up, entity.get()));
-
-        int entity_index = 0;
-        for (int y = 10; y < 10 + 4; ++y) {
-            for (int x = 9; x < 9 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), entity.get());
-
-                // Should count up according to the rules specified in entity_index
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetMultiTileIndex(), entity_index++);
-
-
-                // Ensure tile width and height are properly set
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetDimensions().span, 3);
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetDimensions().height, 4);
-            }
-        }
+        ValidatePlaced({9, 10}, entity_, dimension);
     }
 
     TEST_F(WorldPlacementTest, RemoveEntity3x4Valid) {
-        // Ensure that irregular shaped multi-tiles fully remove
-        const auto entity = std::make_unique<proto::ContainerEntity>();
-        entity->SetWidth(3);
-        entity->SetHeight(4);
-
-        const auto* chunk = world_.GetChunkC({0, 0});
+        const proto::FWorldObject::Dimension dimension(3, 4);
+        entity_.SetDimension(dimension);
 
 
-        EXPECT_TRUE(world_.Place({9, 10}, Orientation::up, entity.get()));
+        EXPECT_TRUE(world_.Place({9, 10}, Orientation::up, &entity_));
         EXPECT_TRUE(world_.Place({9, 13}, Orientation::up, nullptr));
 
-        for (int y = 10; y < 10 + 4; ++y) {
-            for (int x = 9; x < 9 + 3; ++x) {
-                const auto index = y * kChunkWidth + x;
-                EXPECT_EQ(chunk->Tiles(TileLayer::entity)[index].GetPrototype(), nullptr);
-            }
-        }
+        ValidateEmpty({9, 13}, dimension);
     }
 } // namespace jactorio::game
